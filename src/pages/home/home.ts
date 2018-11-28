90cd90074803c4f37a5802e54c31fe87a6ae44cb
Maxim Kamenev <MegaRoks@ya.ru>
2018-11-28 18:52:51 +0600

    Сreated menu

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,4 +11,26 @@ export class HomePage {
 
   }
 
+  OpenPage(id) {
+    switch (id) {
+      case 1:
+        this.navCtrl.push('VariabilityPage');
+        break;
+      case 2:
+        this.navCtrl.push('PressurePage');
+        break;
+      case 3:
+        this.navCtrl.push('PulsePage');
+        break;
+      case 4:
+        this.navCtrl.push('PulseWhileWalkingPage');
+        break;
+      case 5:
+        this.navCtrl.push('SmallPulsePage');
+        break;
+      case 6:
+        this.navCtrl.push('HighPulsePage');
+        break;
+    }
+  }
 }
